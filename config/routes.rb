585e684230306a42baa249ff01a67b09b57ffefb
railsfactory-pavan <pavanrf07@gmail.com>
2019-10-25 13:14:24 +0530

    root to raffle index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'raffle/index'
+  # get 'raffle/index'
+  root to: 'raffle#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
